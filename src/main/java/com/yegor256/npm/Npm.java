7d5c93e5e681df29b5b88820af0aa14e04484806
Sammers21 <titantins@gmail.com>
2019-12-10 14:18:13 +0300

    fixing qulice warnings

--- src/main/java/com/yegor256/npm/Npm.java
+++ src/main/java/com/yegor256/npm/Npm.java
@@ -1,18 +1,18 @@
 /**
  * The MIT License (MIT)
- * <p>
+ *
  * Copyright (c) 2019 Yegor Bugayenko
- * <p>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * <p>
+ *
  * The above copyright notice and this permission notice shall be included
  * in all copies or substantial portions of the Software.
- * <p>
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,14 +23,14 @@
  */
 package com.yegor256.npm;
 
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 
 /**
  * The NPM front. The main goal is to consume a json uploaded by {@code npm publish command} and:
@@ -57,7 +57,7 @@ public class Npm {
      * Publish a new version of an npm package.
      *
      * @param prefix Path prefix for achieves and meta information storage
-     * @param key    Where uploaded json file is stored
+     * @param key Where uploaded json file is stored
      * @throws IOException If fails
      */
     public void publish(final String prefix, final String key) throws IOException {
