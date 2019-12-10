1c29a5d4ece36f626fde044aceb2c3f2105d3a22
Sammers21 <titantins@gmail.com>
2019-12-10 13:53:49 +0300

    fixing qulice warnings

--- src/main/java/com/yegor256/npm/Npm.java
+++ src/main/java/com/yegor256/npm/Npm.java
@@ -1,3 +1,26 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2019 Yegor Bugayenko
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.yegor256.npm;
 
 import javax.json.Json;
@@ -9,35 +32,51 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+/**
+ * The NPM front. The main goal is to consume a json uploaded by {@code npm publish command} and:
+ * 1. to generate source archives
+ * 2. meta.json file
+ */
 public class Npm {
 
+    /**
+     * The storage.
+     */
     private final Storage storage;
 
-    public Npm(Storage storage) {
+    /**
+     * Constructor.
+     *
+     * @param storage The storage
+     */
+    public Npm(final Storage storage) {
         this.storage = storage;
     }
 
     /**
      * Publish a new version of an npm package.
      *
-     * @param prefix path prefix for achieves and meta information storage
-     * @param key    where uploaded json file is stored
+     * @param prefix Path prefix for achieves and meta information storage
+     * @param key    Where uploaded json file is stored
+     * @throws IOException If fails
      */
-    public void publish(String prefix, String key) throws IOException {
-        Path uploadedFile = Files.createTempFile("upload-json", "json");
-        Path metaFile = Files.createTempFile("meta", "json");
-        storage.load(key, uploadedFile);
-        JsonReader reader = Json.createReader(new BufferedInputStream(new FileInputStream(uploadedFile.toFile())));
-        JsonObject uploadedJson = reader.readObject();
-        String metaFileName = prefix + "/meta.json";
-        Meta meta;
-        if (storage.exists(metaFileName)) {
-            storage.load(metaFileName, metaFile);
-            meta = new Meta(metaFile);
-            meta.update(uploadedJson);
+    public void publish(final String prefix, final String key) throws IOException {
+        final Path upload = Files.createTempFile("upload-json", "json");
+        final Path metafile = Files.createTempFile("meta", "json");
+        this.storage.load(key, upload);
+        final JsonReader reader = Json.createReader(
+                new BufferedInputStream(new FileInputStream(upload.toFile()))
+        );
+        final JsonObject uploaded = reader.readObject();
+        final String metafilename = prefix + "/meta.json";
+        final Meta meta;
+        if (this.storage.exists(metafilename)) {
+            this.storage.load(metafilename, metafile);
+            meta = new Meta(metafile);
+            meta.update(uploaded);
         } else {
-            new Meta(uploadedJson, metaFile);
+            new Meta(uploaded, metafile);
         }
-        storage.save(metaFileName, metaFile);
+        this.storage.save(metafilename, metafile);
     }
 }
--- src/test/java/com/yegor256/npm/package-info.java
+++ src/test/java/com/yegor256/npm/package-info.java
@@ -22,29 +22,3 @@
  * SOFTWARE.
  */
 package com.yegor256.npm;
-
-import io.vertx.reactivex.core.Vertx;
-import org.junit.Assert;
-import org.junit.Test;
-
-import java.io.File;
-import java.io.IOException;
-
-public class NPMInstallTest {
-
-    @Test
-    public void nomCommandsPublishAndInstallWorks() throws IOException, InterruptedException {
-        int port = 8080;
-        Storage.Simple storage = new Storage.Simple();
-        NPMRegistry npmRegistry = new NPMRegistry(Vertx.vertx(), storage, port);
-        npmRegistry.start();
-        int publishExitCode = new ProcessBuilder()
-                .directory(new File("./src/test/resources/simple-npm-project/"))
-                .command("npm", "publish", "--registry", "http://127.0.0.1:" + port)
-                .inheritIO()
-                .start()
-                .waitFor();
-        Assert.assertEquals(0, publishExitCode);
-        npmRegistry.stop();
-    }
-}
