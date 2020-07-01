87abb7531412ff70c47d6fa6d2e0ce403da01ddb
Victor Noël <victor.noel@crazydwarves.org>
2020-07-01 19:00:12 +0200

    (#16) - Add todo for tests and fix version

--- npm-proxy-adapter/src/main/java/com/artipie/npm/proxy/CircuitBreakerNpmRemote.java
+++ npm-proxy-adapter/src/main/java/com/artipie/npm/proxy/CircuitBreakerNpmRemote.java
@@ -32,12 +32,18 @@ import java.nio.file.Path;
 
 /**
  * Decorate a {@link NpmRemote} with a {@link CircuitBreaker}.
- * @since 0.2
+ * @since 0.7
  * @todo #16:30min Wrap new instances of HttpNpmRemote with this class
  *  in NpmProxy. But first ensure that HttpNpmRemote throws an exception
  *  in case the request fails (it should still return Maybe for 404
  *  though). Also see https://vertx.io/docs/vertx-circuit-breaker/java/
  *  for configuring the CircuitBreaker.
+ * @todo #16:30min Add a test to ensure it works as expected. The most simple
+ *  is to provide a Fake version of NpmRemote that can be setup to either fail
+ *  or work as expected by the contract of NpmRemote. Be careful about the fact
+ *  that the expected behaviour is beasically: empty if the asset/package is not
+ *  present and an exception if there is an error. See also the todo above or
+ *  HttpNpmRemote if it had been solved already.
  */
 public final class CircuitBreakerNpmRemote implements NpmRemote {
 
