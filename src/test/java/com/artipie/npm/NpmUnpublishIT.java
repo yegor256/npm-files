27ed198bfff9391f3cb52509059e4f0a35747454
Aleksandr Krasnov <genryxy.alexandr@yandex.ru>
2021-01-25 16:44:55 +0300

    #154 - Add IT for npm unpublish

---
+++ src/test/java/com/artipie/npm/NpmUnpublishIT.java
@@ -0,0 +1,133 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 artipie.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.artipie.npm;
+
+import com.artipie.asto.Key;
+import com.artipie.asto.Storage;
+import com.artipie.asto.fs.FileStorage;
+import com.artipie.asto.test.TestResource;
+import com.artipie.http.slice.LoggingSlice;
+import com.artipie.npm.http.NpmSlice;
+import com.artipie.vertx.VertxSliceServer;
+import com.jcabi.log.Logger;
+import io.vertx.reactivex.core.Vertx;
+import java.net.URL;
+import java.nio.file.Path;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.StringContains;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.io.TempDir;
+import org.testcontainers.Testcontainers;
+import org.testcontainers.containers.Container;
+import org.testcontainers.containers.GenericContainer;
+
+/**
+ * IT for `npm unpublish` command.
+ * @since 0.8
+ * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
+ */
+@DisabledOnOs(OS.WINDOWS)
+@SuppressWarnings("PMD.AvoidDuplicateLiterals")
+final class NpmUnpublishIT {
+
+    /**
+     * Temporary directory for all tests.
+     * @checkstyle VisibilityModifierCheck (3 lines)
+     */
+    @TempDir Path tmp;
+
+    /**
+     * Vert.x used to create tested FileStorage.
+     */
+    private Vertx vertx;
+
+    /**
+     * Storage used as repository.
+     */
+    private Storage storage;
+
+    /**
+     * Server.
+     */
+    private VertxSliceServer server;
+
+    /**
+     * Repository URL.
+     */
+    private String url;
+
+    /**
+     * Container.
+     */
+    private GenericContainer<?> cntn;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        this.vertx = Vertx.vertx();
+        this.storage = new FileStorage(this.tmp);
+        final int port = new RandomFreePort().value();
+        this.url = String.format("http://host.testcontainers.internal:%d", port);
+        this.server = new VertxSliceServer(
+            this.vertx,
+            new LoggingSlice(new NpmSlice(new URL(this.url), this.storage)),
+            port
+        );
+        this.server.start();
+        Testcontainers.exposeHostPorts(port);
+        this.cntn = new GenericContainer<>("node:14-alpine")
+            .withCommand("tail", "-f", "/dev/null")
+            .withWorkingDirectory("/home/")
+            .withFileSystemBind(this.tmp.toString(), "/home");
+        this.cntn.start();
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.server.stop();
+        this.vertx.close();
+        this.cntn.stop();
+    }
+
+    @Test
+    void npmUnpublishWorks() throws Exception {
+        final String proj = "@hello/simple-npm-project";
+        new TestResource("storage")
+            .addFilesTo(this.storage, Key.ROOT);
+        MatcherAssert.assertThat(
+            this.exec("npm", "unpublish", proj, "--force", "--registry", this.url),
+            new StringContains(String.format("- %s", proj))
+        );
+    }
+
+    private String exec(final String... command) throws Exception {
+        Logger.debug(this, "Command:\n%s\n", String.join(" ", command));
+        final Container.ExecResult res = this.cntn.execInContainer(command);
+        Logger.debug(this, "STDOUT:\n%s\nSTDERR:\n%s", res.getStdout(), res.getStderr());
+        return this.cntn.execInContainer(command).getStdout();
+    }
+}
